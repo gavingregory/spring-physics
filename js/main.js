1c203a12edd6cc12ac4c3e3232010303581dbbb3
Gavin Gregory <gavin.ian.gregory@gmail.com>
2016-05-21 21:28:04 +0100

    Moved physics from models to src.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 var Entity = require('./models/entity')
   , StaticEntity = require('./models/static-entity')
   , Spring = require('./models/spring')
-  , physics = require('./models/physics');
+  , physics = require('./src/physics');
 
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 10000 );
